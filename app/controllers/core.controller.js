af6782713c4522dc28a97167d3fa0623200d58ae
Yuan Zhi <yuzhi@microsoft.com>
2015-02-10 20:50:21 -0500

    use json instead of send to response json obj

--- app/controllers/core.controller.js
+++ app/controllers/core.controller.js
@@ -7,6 +7,6 @@ var config = require('../../config/config');
 var util = require('util');
 
 exports.index = function(req, res){
-    var json = util.format('{ "title":"%s", "version":"%s" }', config.app.title, config.app.dbVersion);
-    res.send(json);
+    var json = { title: config.app.title, version: config.app.dbVersion };
+    res.json(json);
 };
